8e4fb4c354997102ca5da58e52473a009a41be77
Thominik <dominikbednarz.ti@gmail.com>
2023-06-23 16:13:07 +0200

    Fix 2

--- sing_adapt_w61905/Program.cs
+++ sing_adapt_w61905/Program.cs
@@ -8,17 +8,32 @@ IReservationAdapter adapter = new ReservationAdapter();
 ReservationManager manager = ReservationManager.GetInstance(null);
 manager.SetAdapter(adapter);
 
+Console.WriteLine("Witaj w Hotelu Turion, potrzebujemy kilka informacji, aby dokonać rezerwacji.");
 Console.Write("Podaj numer pokoju: ");
 string roomNumber = Console.ReadLine();
 
-Console.Write("Podaj nazwisko gościa: ");
+Console.Write("Podaj imię i nazwisko gościa: ");
 string guestName = Console.ReadLine();
 
 Console.Write("Podaj datę przyjazdu (dd.MM.yyyy): ");
 DateTime arrivalDate = DateTime.ParseExact(Console.ReadLine(), "dd.MM.yyyy", CultureInfo.InvariantCulture);
 
+if (arrivalDate < DateTime.Today)
+{
+    Console.WriteLine("Nie można dokonać rezerwacji na datę z przeszłości.");
+    Console.WriteLine("Spróbuj ponownie.");
+    arrivalDate = DateTime.ParseExact(Console.ReadLine(), "dd.MM.yyyy", CultureInfo.InvariantCulture);
+}
+
 Console.Write("Podaj datę wyjazdu (dd.MM.yyyy): ");
 DateTime departureDate = DateTime.ParseExact(Console.ReadLine(), "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+if (departureDate > DateTime.Today.AddMonths(12))
+{
+    Console.WriteLine("Nie można dokonać rezerwacji na termin późniejszy niż rok od dzisiaj.");
+    Console.WriteLine("Spróbuj ponownie.");
+    departureDate = DateTime.ParseExact(Console.ReadLine(), "dd.MM.yyyy", CultureInfo.InvariantCulture);
+}
 
 Console.Write("Wybierz typ pokoju (1 - standard, 2 - VIP): ");
 int roomType = int.Parse(Console.ReadLine());
@@ -51,3 +66,16 @@ else
 
 manager.MakeReservation(reservation);
 manager.DisplayReservations();
+
+Console.WriteLine("Dodatkowe usługi: ");
+
+if (roomType != 1)
+{
+    Console.Write( ((VipRoom)reservation.Room).ExtraServices);
+}
+else
+{
+    Console.Write("Brak dodatkowych usług.");
+}
+
+Console.WriteLine("\nDziękujemy za dokonanie rezerwacji.");
